f4ef794eacd2650eff566994480496eae0e9c6b4
Maddox-RVS <rynvnskl@yahoo.com>
2026-07-30 15:31:39 -0400

    Untracked examples directory.

--- examples/intro.py
+++
@@ -1,7 +0,0 @@
-from pyclack.prompts import intro
-
-def main() -> None:
-    intro('Welcome to the Intro Example')
-
-if __name__ == '__main__':
-    main()
--- examples/outro.py
+++
@@ -1,8 +0,0 @@
-from pyclack.prompts import intro, outro
-
-def main() -> None:
-    intro('Welcome to the Outro Example')
-    outro('Goodbye! 👋')
-
-if __name__ == '__main__':
-    main()
