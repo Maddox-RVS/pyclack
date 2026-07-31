baff67c72a8f6f7aab898b92ef59cb21812f222a
Maddox-RVS <rynvnskl@yahoo.com>
2026-07-31 19:17:58 -0400

    Overhauled prompr.ask using new TextBoxController class, allows for more rpecise cursor control (up/down), although unlikley used since enter submits.

--- src/pyclack/prompts/ask.py
+++ src/pyclack/prompts/ask.py
@@ -1,8 +1,8 @@
 from ..renderer import RenderFrame, Text, FrameBuilder, Style, Theme
+from .util import build_wrapped_input_lines, TextBoxController
 from .prompt_base import PromptBase, CancelException
 from typing import Callable, Optional, override
 from ..terminal import CursorController as cc
-from .util import build_wrapped_input_lines
 from ..config import get_active_theme
 from ..terminal import Stdout
 from copy import copy
@@ -17,7 +17,7 @@ def ask(message: str,
     '''
     
     prompt: Ask = Ask(message, cancellation_message, placeholder, initial_value, validate)
-    return prompt.input_buffer
+    return prompt.text_box_controller.get_input()
 
 class Ask(PromptBase):
     def __init__(self,
@@ -39,8 +39,10 @@ class Ask(PromptBase):
         self.validate: Optional[Callable[[str], Optional[str]]] = validate
 
         self.render_frame: RenderFrame = RenderFrame()
-        self.input_buffer: str = initial_value if initial_value is not None else ''
-        self.input_index: int = 0 if initial_value is None else len(initial_value)
+        self.text_box_controller: TextBoxController = TextBoxController()
+        if initial_value: 
+            self.text_box_controller.set_input(initial_value, 0)
+            self.text_box_controller.cursor_end()
         
         self.propogate_key_after_error = True
         self.allowed_inputs: tuple[str] = self._construct_allowed_inputs()
@@ -53,7 +55,7 @@ class Ask(PromptBase):
         '''
 
         allowed_chars: tuple[str] = tuple(chr(i) for i in range(32, 127))
-        return ('BACKSPACE', 'ENTER', 'LEFT', 'RIGHT', 'TAB', 'SPACE') + allowed_chars
+        return ('BACKSPACE', 'ENTER', 'LEFT', 'RIGHT', 'UP', 'DOWN', 'TAB', 'SPACE') + allowed_chars
 
     @override
     def handle_active(self, key: Optional[str]) -> bool:
@@ -66,31 +68,32 @@ class Ask(PromptBase):
         connector_bar_end: str = theme.symbols.connector_bar_end.resolve()
 
         # Update the input buffer based on the key pressed
-        if key == 'BACKSPACE': # Remove character at input_index in input_buffer
-            self.input_buffer = self.input_buffer[:max(0, self.input_index - 1)] + self.input_buffer[self.input_index:]
-            self.input_index = max(0, self.input_index - 1)
+        if key == 'BACKSPACE': self.text_box_controller.delete()
         elif key == 'ENTER': return True # Advance to the next state (submit)
-        elif key == 'LEFT': self.input_index = max(0, self.input_index - 1) # Move input_index once to the left
-        elif key == 'RIGHT': self.input_index = min(len(self.input_buffer), self.input_index + 1) # Move input_index once to the right
+        elif key == 'LEFT': self.text_box_controller.cursor_left()
+        elif key == 'RIGHT': self.text_box_controller.cursor_right()
+        elif key == 'UP': self.text_box_controller.cursor_up()
+        elif key == 'DOWN': self.text_box_controller.cursor_down()
         else:
             map: dict[str, str] = {
                 'SPACE': ' ',
                 'TAB': '\t'}  
             char: str = map.get(key, key) # Translate key to character (if applicable)
-            if key != '':
-                self.input_buffer = self.input_buffer[:self.input_index] + char + self.input_buffer[self.input_index:]
-                self.input_index = min(len(self.input_buffer), self.input_index + 1)
+            if key != '': self.text_box_controller.insert(char)
 
         # Create and render next frame based on the current input buffer and state
+        input_buffer: str = self.text_box_controller.get_input()
+        input_index: int = self.text_box_controller.get_cursor_position()
+
         frame_builder: FrameBuilder = FrameBuilder()
         message_lines: list[Text] = build_wrapped_input_lines(self.message, 0, theme.text, theme.active)
         message_lines[0] = Text(step_marker_active, Text(message_lines[0].get_raw_text()[1:], style=theme.text), style=theme.active)
         frame_builder.add_lines(*message_lines)
  
-        if self.placeholder and len(self.input_buffer) <= 0:
+        if self.placeholder and len(input_buffer) <= 0:
             frame_builder.add_lines(*build_wrapped_input_lines(self.placeholder, 0, theme.muted, theme.active, True))
         else:
-            frame_builder.add_lines(*build_wrapped_input_lines(self.input_buffer, self.input_index, theme.text, theme.active, True))
+            frame_builder.add_lines(*build_wrapped_input_lines(input_buffer, input_index, theme.text, theme.active, True))
  
         frame_builder.add_line(Text(connector_bar_end, style=theme.active))
  
@@ -103,17 +106,20 @@ class Ask(PromptBase):
         theme: Theme = get_active_theme()
         step_marker_submit: str = theme.symbols.step_marker_submit.resolve()
 
+        input_buffer: str = self.text_box_controller.get_input()
+        input_index: int = self.text_box_controller.get_cursor_position()
+
         # Create and render next frame based on the current input buffer and state
         frame_builder: FrameBuilder = FrameBuilder()
         message_lines: list[Text] =  build_wrapped_input_lines(self.message, 0, theme.text, theme.muted)
         message_lines[0] = Text(step_marker_submit, Text(message_lines[0].get_raw_text()[1:], style=theme.text), style=theme.submit)
         frame_builder.add_lines(*message_lines)
-        frame_builder.add_lines(*build_wrapped_input_lines(self.input_buffer, self.input_index, theme.muted, theme.muted))
+        frame_builder.add_lines(*build_wrapped_input_lines(input_buffer, input_index, theme.muted, theme.muted))
         frame: tuple[Text, ...] = frame_builder.build()
         self.render_frame.draw_frame(*frame)
 
         Stdout.put(cc.show_cursor())
-        return False if self.validate(self.input_buffer) else True
+        return False if self.validate(input_buffer) else True
 
     @override
     def handle_error(self, key: Optional[str]) -> bool:
@@ -123,17 +129,20 @@ class Ask(PromptBase):
         step_marker_error: str = theme.symbols.step_marker_error.resolve()
         connector_bar_end: str = theme.symbols.connector_bar_end.resolve()
 
+        input_buffer: str = self.text_box_controller.get_input()
+        input_index: int = self.text_box_controller.get_cursor_position()
+
         # Create and render next frame based on the current input buffer and state
         frame_builder: FrameBuilder = FrameBuilder()
         message_lines: list[Text] = build_wrapped_input_lines(self.message, 0, theme.text, theme.error)
         message_lines[0] = Text(step_marker_error, Text(message_lines[0].get_raw_text()[1:], style=theme.text), style=theme.error)
         frame_builder.add_lines(*message_lines)
-        if self.placeholder and len(self.input_buffer) <= 0:
+        if self.placeholder and len(input_buffer) <= 0:
             frame_builder.add_lines(*build_wrapped_input_lines(self.placeholder, 0, theme.muted, theme.error, True))
         else:
-            frame_builder.add_lines(*build_wrapped_input_lines(self.input_buffer, self.input_index, theme.text, theme.error, True))
+            frame_builder.add_lines(*build_wrapped_input_lines(input_buffer, input_index, theme.text, theme.error, True))
 
-        validation_error_message: str = self.validate(self.input_buffer)
+        validation_error_message: str = self.validate(input_buffer)
         error_lines: list[Text] = build_wrapped_input_lines(validation_error_message, 0, theme.error, theme.error)
         last_index: int = len(error_lines) - 1
         error_lines[last_index] = Text(connector_bar_end, Text(error_lines[last_index].get_raw_text()[1:], style=theme.error), style=theme.error)
@@ -152,6 +161,9 @@ class Ask(PromptBase):
         connector_bar_vertical: str = theme.symbols.connector_bar_vertical.resolve()
         connector_bar_end: str = theme.symbols.connector_bar_end.resolve()
 
+        input_buffer: str = self.text_box_controller.get_input()
+        input_index: int = self.text_box_controller.get_cursor_position()
+
         # Create and render next frame based on the current input buffer and state
         frame_builder: FrameBuilder = FrameBuilder()
         message_lines: list[Text] =  build_wrapped_input_lines(self.message, 0, theme.text, theme.muted)
@@ -160,8 +172,8 @@ class Ask(PromptBase):
 
         text_style: Style = copy(theme.muted)
         text_style.strikethrough = True
-        if len(self.input_buffer) > 0:
-            frame_builder.add_lines(*build_wrapped_input_lines(self.input_buffer, self.input_index, text_style, theme.muted))
+        if len(input_buffer) > 0:
+            frame_builder.add_lines(*build_wrapped_input_lines(input_buffer, input_index, text_style, theme.muted))
         frame_builder.add_line(Text(connector_bar_vertical, style=theme.muted))
         cancel_lines: list[Text] = build_wrapped_input_lines(self.cancellation_message, 0, theme.cancel, theme.muted)
         last_index: int = len(cancel_lines) - 1
@@ -171,4 +183,4 @@ class Ask(PromptBase):
         self.render_frame.draw_frame(*frame)
 
         Stdout.put(cc.show_cursor())
-        raise CancelException(self.cancellation_message, self.input_buffer)
+        raise CancelException(self.cancellation_message, input_buffer)
