2e7bd791fb54bfd1b2ecf664b67327c4906c9e7b
Maddox-RVS <rynvnskl@yahoo.com>
2026-07-31 21:15:49 -0400

    Added password prompt. Fixed smalled bug in ask prompt.

--- src/pyclack/prompts/__init__.py
+++ src/pyclack/prompts/__init__.py
@@ -1,4 +1,8 @@
 from .prompt_base import PromptBase, PromptState, CancelException
+from . import util
+
+# Prompts
 from .ask import ask
 from .intro import intro
 from .outro import outro
+from .password import password
--- src/pyclack/prompts/password.py
+++ src/pyclack/prompts/password.py
@@ -1,4 +1,4 @@
-from ..renderer import RenderFrame, Text, FrameBuilder, Style, Theme
+from ..renderer import RenderFrame, Text, FrameBuilder, Style, Theme, Symbol
 from .util import build_wrapped_input_lines, TextBoxController
 from .prompt_base import PromptBase, CancelException
 from typing import Callable, Optional, override
@@ -7,47 +7,47 @@ from ..config import get_active_theme
 from ..terminal import Stdout
 from copy import copy
 
-def ask(message: str, 
-        placeholder: Optional[str] = None, 
-        initial_value: Optional[str] = None, 
+def password(message: str, 
+        mask: Optional[Symbol] = None,
+        show_nothing: bool = False,
         validate: Optional[Callable[[str], Optional[str]]] = None,
         cancellation_message: str = 'Operation Cancelled') -> str:
     '''
-    Ask the user for input with a message, placeholder, initial value, and validation function.
+    Ask the user for input as a password.
 
     Args:
         message (str): The message to display to the user.
-        placeholder (str, optional): The placeholder text to display when the input is empty.
-        initial_value (str, optional): The initial value of the input.
-        validate (Callable[[str], Optional[str]], optional): A function to validate the input.
+        mask (Symbol, optional): The symbol to use for masking the input. Defaults to None
+        show_nothing (bool, optional): If True, the input will not be displayed at all. Defaults to False.
+        validate (Callable[[str], Optional[str]], optional): A function to validate the input. Defaults to None.
         cancellation_message (str): The message to display if the user cancels the operation.
 
     Returns:
-        str: The user's input.
+        str: The user's input as a password.
 
     Raises:
         CancelException: If the user cancels the operation.
     '''
     
-    prompt: Ask = Ask(message, cancellation_message, placeholder, initial_value, validate)
+    prompt: Password = Password(message, cancellation_message, mask, show_nothing, validate)
     return prompt.text_box_controller.get_input()
 
-class Ask(PromptBase):
+class Password(PromptBase):
     def __init__(self,
             message: str,
             cancellation_message: str,
-            placeholder: Optional[str] = None,
-            initial_value: Optional[str] = None,
+            mask: Optional[Symbol] = None,
+            show_nothing: bool = False,
             validate: Optional[Callable[[str], Optional[str]]] = None):
         '''
-        Initialize an Ask prompt with the given message, placeholder, initial value, and validation function.
+        Initialize a Password prompt with the given message, mask, and validation function.
 
         Args:
             message (str): The message to display to the user.
             cancellation_message (str): The message to display if the user cancels the operation.
-            placeholder (str, optional): The placeholder text to display when the input is empty.
-            initial_value (str, optional): The initial value of the input.
-            validate (Callable[[str], Optional[str]], optional): A function to validate the input.
+            mask (Symbol, optional): The symbol to use for masking the input. Defaults to None.
+            show_nothing (bool, optional): If True, the input will not be displayed at all. Defaults to False.
+            validate (Callable[[str], Optional[str]], optional): A function to validate the input. Defaults to None.
 
         Raises:
             CancelException: If the user cancels the operation.
@@ -57,15 +57,12 @@ class Ask(PromptBase):
 
         self.message: str = message
         self.cancellation_message: str = cancellation_message
-        self.placeholder: Optional[str] = placeholder
-        self.initial_value: Optional[str] = initial_value
+        self.mask: Optional[Symbol] = mask
+        self.show_nothing: bool = show_nothing
         self.validate: Optional[Callable[[str], Optional[str]]] = validate
 
         self.render_frame: RenderFrame = RenderFrame()
         self.text_box_controller: TextBoxController = TextBoxController()
-        if initial_value: 
-            self.text_box_controller.set_input(initial_value, 0)
-            self.text_box_controller.cursor_end()
         
         self.propogate_key_after_error = True
         self.allowed_inputs: tuple[str] = self._construct_allowed_inputs()
@@ -78,7 +75,7 @@ class Ask(PromptBase):
         '''
 
         allowed_chars: tuple[str] = tuple(chr(i) for i in range(32, 127))
-        return ('BACKSPACE', 'ENTER', 'LEFT', 'RIGHT', 'UP', 'DOWN', 'TAB', 'SPACE') + allowed_chars
+        return ('BACKSPACE', 'ENTER', 'TAB', 'SPACE') + allowed_chars
 
     @override
     def handle_active(self, key: Optional[str]) -> bool:
@@ -93,10 +90,6 @@ class Ask(PromptBase):
         # Update the input buffer based on the key pressed
         if key == 'BACKSPACE': self.text_box_controller.delete()
         elif key == 'ENTER': return True # Advance to the next state (submit)
-        elif key == 'LEFT': self.text_box_controller.cursor_left()
-        elif key == 'RIGHT': self.text_box_controller.cursor_right()
-        elif key == 'UP': self.text_box_controller.cursor_up()
-        elif key == 'DOWN': self.text_box_controller.cursor_down()
         else:
             map: dict[str, str] = {
                 'SPACE': ' ',
@@ -105,21 +98,16 @@ class Ask(PromptBase):
             if key != '': self.text_box_controller.insert(char)
 
         # Create and render next frame based on the current input buffer and state
-        input_buffer: str = self.text_box_controller.get_input()
+        mask: str = self.mask.resolve() if self.mask else theme.symbols.selection_widget_password_mask.resolve()
+        input_buffer: str = mask * len(self.text_box_controller.get_input()) if not self.show_nothing else ''
         input_index: int = self.text_box_controller.get_cursor_position()
 
         frame_builder: FrameBuilder = FrameBuilder()
         message_lines: list[Text] = build_wrapped_input_lines(self.message, 0, theme.text, theme.active)
         message_lines[0] = Text(step_marker_active, Text(message_lines[0].get_raw_text()[1:], style=theme.text), style=theme.active)
         frame_builder.add_lines(*message_lines)
- 
-        if self.placeholder and len(input_buffer) <= 0:
-            frame_builder.add_lines(*build_wrapped_input_lines(self.placeholder, 0, theme.muted, theme.active, True))
-        else:
-            frame_builder.add_lines(*build_wrapped_input_lines(input_buffer, input_index, theme.text, theme.active, True))
- 
+        frame_builder.add_lines(*build_wrapped_input_lines(input_buffer, input_index, theme.text, theme.active, not self.show_nothing))
         frame_builder.add_line(Text(connector_bar_end, style=theme.active))
- 
         frame: tuple[Text, ...] = frame_builder.build()
         self.render_frame.draw_frame(*frame)
         return False
@@ -130,7 +118,8 @@ class Ask(PromptBase):
         step_marker_submit: str = theme.symbols.step_marker_submit.resolve()
 
         # Create and render next frame based on the current input buffer and state
-        input_buffer: str = self.text_box_controller.get_input()
+        mask: str = self.mask.resolve() if self.mask else theme.symbols.selection_widget_password_mask.resolve()
+        input_buffer: str = mask * len(self.text_box_controller.get_input()) if not self.show_nothing else ''
         input_index: int = self.text_box_controller.get_cursor_position()
 
         frame_builder: FrameBuilder = FrameBuilder()
@@ -142,7 +131,7 @@ class Ask(PromptBase):
         self.render_frame.draw_frame(*frame)
 
         Stdout.put(cc.show_cursor())
-        return False if self.validate and self.validate(input_buffer) else True
+        return False if self.validate and self.validate(self.text_box_controller.get_input()) else True
 
     @override
     def handle_error(self, key: Optional[str]) -> bool:
@@ -153,19 +142,16 @@ class Ask(PromptBase):
         connector_bar_end: str = theme.symbols.connector_bar_end.resolve()
 
         # Create and render next frame based on the current input buffer and state
-        input_buffer: str = self.text_box_controller.get_input()
+        mask: str = self.mask.resolve() if self.mask else theme.symbols.selection_widget_password_mask.resolve()
+        input_buffer: str = mask * len(self.text_box_controller.get_input()) if not self.show_nothing else ''
         input_index: int = self.text_box_controller.get_cursor_position()
 
         frame_builder: FrameBuilder = FrameBuilder()
         message_lines: list[Text] = build_wrapped_input_lines(self.message, 0, theme.text, theme.error)
         message_lines[0] = Text(step_marker_error, Text(message_lines[0].get_raw_text()[1:], style=theme.text), style=theme.error)
         frame_builder.add_lines(*message_lines)
-        if self.placeholder and len(input_buffer) <= 0:
-            frame_builder.add_lines(*build_wrapped_input_lines(self.placeholder, 0, theme.muted, theme.error, True))
-        else:
-            frame_builder.add_lines(*build_wrapped_input_lines(input_buffer, input_index, theme.text, theme.error, True))
-
-        validation_error_message: str = self.validate(input_buffer) # self.validate must be defined if we are in the error state
+        frame_builder.add_lines(*build_wrapped_input_lines(input_buffer, input_index, theme.text, theme.error, not self.show_nothing))
+        validation_error_message: str = self.validate(self.text_box_controller.get_input()) # self.validate must be defined if we are in the error state
         error_lines: list[Text] = build_wrapped_input_lines(validation_error_message, 0, theme.error, theme.error)
         last_index: int = len(error_lines) - 1
         error_lines[last_index] = Text(connector_bar_end, Text(error_lines[last_index].get_raw_text()[1:], style=theme.error), style=theme.error)
@@ -185,7 +171,8 @@ class Ask(PromptBase):
         connector_bar_end: str = theme.symbols.connector_bar_end.resolve()
 
         # Create and render next frame based on the current input buffer and state
-        input_buffer: str = self.text_box_controller.get_input()
+        mask: str = self.mask.resolve() if self.mask else theme.symbols.selection_widget_password_mask.resolve()
+        input_buffer: str = mask * len(self.text_box_controller.get_input()) if not self.show_nothing else ''
         input_index: int = self.text_box_controller.get_cursor_position()
 
         frame_builder: FrameBuilder = FrameBuilder()
@@ -206,4 +193,4 @@ class Ask(PromptBase):
         self.render_frame.draw_frame(*frame)
 
         Stdout.put(cc.show_cursor())
-        raise CancelException(self.cancellation_message, input_buffer)
+        raise CancelException(self.cancellation_message, self.text_box_controller.get_input())
