8d7d0dd2e14d94c63616c2ef0c61c22b1850f60a
Maddox-RVS <rynvnskl@yahoo.com>
2026-08-03 02:52:25 -0400

    Updated pyproject.toml. Wrote entrypoint for date prompt.

--- src/pyclack/prompts/date.py
+++ src/pyclack/prompts/date.py
@@ -3,30 +3,40 @@ from ..prompts.util import build_wrapped_input_lines
 from ..config import get_active_theme
 from typing import Optional, Callable
 from ..prompts import PromptBase
-from datetime import date
+from datetime import date as ddate
 
 def date(message: str, 
-        initial_date: date, 
-        min_date: date, 
-        max_date: date, 
+        initial_date: ddate, 
+        min_date: ddate, 
+        max_date: ddate, 
         cancellation_message: str = 'Operation Cancelled',
-        default_date: Optional[date],
-        validate: Optional[Callable[[date], Optional[str]]] = None) -> date:
-    pass
+        default_date: Optional[ddate] = None,
+        validate: Optional[Callable[[ddate], Optional[str]]] = None) -> ddate:
+    prompt: Date = Date(message, initial_date, min_date, max_date, cancellation_message, default_date, validate)
+    return prompt.selected_date
 
 class Date(PromptBase):
-    def __init__(self):
+    def __init__(self,
+                message: str,
+                initial_date: ddate,
+                min_date: ddate,
+                max_date: ddate,
+                cancellation_message: str = 'Operation Cancelled',
+                default_date: Optional[ddate] = None,
+                validate: Optional[Callable[[ddate], Optional[str]]] = None):
         super().__init__()
+
+        self.selected_date: ddate = initial_date
 
         super().activate()
 
-    def handle_active(self, key: str) -> bool:
+    def handle_active(self, key: Optional[str]) -> bool:
         pass
 
     def handle_submit(self) -> bool:
         pass
 
-    def handle_error(self, key: str) -> bool:
+    def handle_error(self, key: Optional[str]) -> bool:
         pass
 
     def handle_cancel(self):
