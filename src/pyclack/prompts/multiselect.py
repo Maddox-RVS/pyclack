d5c1d6a352e40661d157a0371c9305050ad9d616
Maddox-RVS <rynvnskl@yahoo.com>
2026-08-16 18:50:30 -0400

    Added default_values param to multiselect prompt.

--- src/pyclack/prompts/multiselect.py
+++ src/pyclack/prompts/multiselect.py
@@ -14,9 +14,10 @@ def multiselect(
     max_items: int = 7,
     cancellation_message: str = 'Operation Cancelled',
     show_cancellation_message: bool = True,
-    abort_time: Optional[float] = None) -> list[ClackOption]:
+    abort_time: Optional[float] = None,
+    default_values: Optional[list[ClackOption]] = None) -> list[ClackOption]:
 
-    prompt: Multiselect = Multiselect(message, cancellation_message, options, show_instructions, max_items, show_cancellation_message, abort_time)
+    prompt: Multiselect = Multiselect(message, cancellation_message, options, show_instructions, max_items, show_cancellation_message, abort_time, default_values)
     selected_options: list[ClackOption] = [prompt.options[index] for index in prompt.selected_options]
     return selected_options
 
@@ -28,7 +29,8 @@ class Multiselect(PromptBase):
         show_instructions: bool, 
         max_items: int,
         show_cancellation_message: bool,
-        abort_time: Optional[float]):
+        abort_time: Optional[float],
+        default_values: Optional[list[ClackOption]]):
 
         super().__init__()
             
@@ -38,6 +40,7 @@ class Multiselect(PromptBase):
         self.show_instructions: bool = show_instructions
         self.max_items: int = max(5, max_items)
         self.show_cancellation_message: bool = show_cancellation_message
+        self.default_values: Optional[list[ClackOption]] = default_values
 
         if len(self.options) == 0:
             raise RuntimeError('Options cannot be empty')
@@ -397,5 +400,5 @@ class Multiselect(PromptBase):
         self.render_frame.draw_frame(*frame)
 
         Stdout.put(cc.show_cursor())
-        selected_options: list[ClackOption] = [self.options[index] for index in self.selected_options]
+        selected_options: list[ClackOption] = [self.options[index] for index in self.selected_options] if not self.default_values else self.default_values
         raise CancelException[list[ClackOption]](self.cancellation_message, selected_options)
