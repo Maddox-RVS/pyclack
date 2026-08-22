eaba91a2712b58110fc2b9e132fd5ccc89d71ca4
Maddox-RVS <rynvnskl@yahoo.com>
2026-08-21 22:31:50 -0400

    Removed unecessary overhead from intro widget implimentation,

--- src/pyclack/widgets/intro.py
+++ src/pyclack/widgets/intro.py
@@ -1,10 +1,8 @@
 from ..renderer import Theme, RenderFrame, Text, FrameBuilder, Style
 from ..prompts.util import build_message_open
 from ..terminal import CursorController as cc
-from ..prompts.prompt_base import PromptBase
 from ..config import get_active_theme
 from ..terminal import Stdout
-from typing import override
 
 def intro(title: str, custom_style: Style | None = None) -> None:
     '''
@@ -14,46 +12,22 @@ def intro(title: str, custom_style: Style | None = None) -> None:
         title (str): The title to display to the user.
         custom_style (Style | None, optional): The custom style to use for the intro.
     '''
-    
-    Intro(title, custom_style)
 
-class Intro(PromptBase):
-    '''
-    A class to display an introductory message with a title.
-    '''
+    Stdout.put(cc.hide_cursor())
+       
+    render_frame: RenderFrame = RenderFrame()
+            
+    theme: Theme = get_active_theme()
+    connector_bar_start: str = theme.symbols.connector_bar_start.resolve()
+    connector_bar_vertical: str = theme.symbols.connector_bar_vertical.resolve()
+    prefix: Text = Text(f'{connector_bar_vertical}  ', theme.muted)
+    opening_prefix: Text = Text(f'{connector_bar_start}  ', theme.muted)
 
-    def __init__(self, title: str, custom_style: Style | None):
-        '''
-        Initialize an Intro prompt with the given title.
+    text_style: Style = custom_style if custom_style else theme.text
+    frame_builder: FrameBuilder = FrameBuilder()
+    title_text_lines: list[Text] = build_message_open(title, text_style, prefix, opening_prefix)
+    frame_builder.add_lines(*title_text_lines)
+    frame: tuple[Text, ...] = frame_builder.build()
+    render_frame.draw_frame(*frame)
 
-        Args:
-            title (str): The title to display to the user.
-            custom_style (Style | None): The custom style to use for the intro.
-        '''
-
-        self.title: str = title
-        self.custom_style: Style | None = custom_style
-        self.render_frame: RenderFrame = RenderFrame()
-
-        super().__init__()
-        super().activate()
-
-    @override
-    def handle_active(self, key: str | None) -> bool:
-        Stdout.put(cc.hide_cursor())
-        
-        theme: Theme = get_active_theme()
-        connector_bar_start: str = theme.symbols.connector_bar_start.resolve()
-        connector_bar_vertical: str = theme.symbols.connector_bar_vertical.resolve()
-        prefix: Text = Text(f'{connector_bar_vertical}  ', theme.muted)
-        opening_prefix: Text = Text(f'{connector_bar_start}  ', theme.muted)
-
-        text_style: Style = self.custom_style if self.custom_style else theme.text
-        frame_builder: FrameBuilder = FrameBuilder()
-        title_text_lines: list[Text] = build_message_open(self.title, text_style, prefix, opening_prefix)
-        frame_builder.add_lines(*title_text_lines)
-        frame: tuple[Text, ...] = frame_builder.build()
-        self.render_frame.draw_frame(*frame)
-
-        Stdout.put(cc.show_cursor())
-        return True
+    Stdout.put(cc.show_cursor())
